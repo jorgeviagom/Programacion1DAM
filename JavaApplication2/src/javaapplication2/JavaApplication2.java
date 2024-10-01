e056aa9e2995fc470d731b4b145ff1fa0e0d5f32
jorgeviagom <jvg1020@hotmail.com>
2024-10-01 21:00:07 +0200

    Update JavaApplication2.java

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -197,7 +197,26 @@ Scanner sc = new Scanner (System.in);
         
         System.out.println("¿Puede salir a la calle?: " + puedeSalir);
 */
-        
+/*
+Un economista te ha encargado un programa para realizar cálculos con el IVA. 
+La aplicación debe solicitar la base imponible y el IVA que se debe aplicar. 
+Muestra en pantalla el importe correspondiente al IVA y el total
+*/
+/*Scanner sc = new Scanner(System.in);
+//Solicitamos al usuario la base imponible
+double baseImponible;
+System.out.println("Introduce la base imponible: ");
+baseImponible = sc.nextDouble();
+//Solicitamos al usuario el porcentaje de IVA
+double iva;
+System.out.println("Introduce el IVA: ");
+iva = sc.nextDouble();
+//Sumamos ambos importes para que nos de el importe total del producto
+double importeTotal;
+importeTotal = baseImponible * (iva/100) + baseImponible;
+//Le decimos al usuario el importe total del producto
+System.out.println("El importe total es: " + importeTotal);
+*/
     }
     
 }
